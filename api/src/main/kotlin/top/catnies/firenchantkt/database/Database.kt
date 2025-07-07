6e5981620ff823c33a7ee3a43b1b587bea640e2c
Catnies <catnies@foxmail.com>
2025-07-07 15:36:41 +0800

    更新依赖

--- api/src/main/kotlin/top/catnies/firenchantkt/database/Database.kt
+++ api/src/main/kotlin/top/catnies/firenchantkt/database/Database.kt
@@ -6,13 +6,16 @@ import org.jetbrains.annotations.NotNull
 // 数据库接口
 interface Database {
 
+    /* 记录使用附魔书的成功和失败历史记录 */
+
+    /* 获取使用附魔书的成功和失败历史记录 */
+
     /* 获取附魔台的种子 */
     fun getEnchantTableSeed(@NotNull player: Player): Int
 
     /* 设置附魔台的种子 */
     fun setEnchantTableSeed(@NotNull player: Player, @NotNull seed: Long)
 
-    /* 刷新附魔台的种子 */
-    fun refreshEnchantTableSeed(@NotNull player: Player)
+
 
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,9 +2,10 @@ dependencies {
     implementation(project(":api"))
     implementation(project(":compatibility"))
 
-    implementation("xyz.xenondevs.invui:invui:1.46") // InvUI
+    compileOnly("io.lettuce:lettuce-core:6.5.3.RELEASE") // Lettuce
 
     compileOnly("me.clip:placeholderapi:2.11.6") // PlaceholderAPI
+    implementation("xyz.xenondevs.invui:invui:1.46") // InvUI
 }
 
 // 确保 plugin.yml 被正确处理
