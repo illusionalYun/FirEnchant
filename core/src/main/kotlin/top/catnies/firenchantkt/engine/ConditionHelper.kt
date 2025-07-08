3800bd1045268bc57dafda39d0687d2734ad6459
Catnies <catnies@foxmail.com>
2025-07-08 23:08:11 +0800

    更新 ConditionHelper

--- core/src/main/kotlin/top/catnies/firenchantkt/engine/ConditionHelper.kt
+++ core/src/main/kotlin/top/catnies/firenchantkt/engine/ConditionHelper.kt
@@ -8,18 +8,14 @@ object ConditionHelper {
 
     fun CommandSender.checkCondition(type: String, args: MutableMap<String, Any>): Boolean {
         args["user"] = this
-        var finalType = type
 
-        var not = false
-        if (type.startsWith('!')) {
-            not = true
-            finalType = type.substring(1)
-        }
+        // 如果type开头是! , 就返回 substring + true, 否则 原字符串 + false
+        val (finalType, isNot) = if (type.startsWith('!')) (type.substring(1) to true) else (type to false)
 
         val conditionClass = FirEnchantAPI.conditionRegistry().getCondition(finalType) ?: return false
         val condition = conditionClass.getDeclaredConstructor(args.javaClass).newInstance(args)
 
-        return not != condition.check()
+        return isNot != condition.check()
     }
 
     fun CommandSender.checkCondition(config: ConfigurationSection): Boolean {
@@ -27,7 +23,6 @@ object ConditionHelper {
         val args = config.getKeys(false).associateWith { config.get(it)!! }.toMutableMap()
         return checkCondition(type, args)
     }
-
 
     fun CommandSender.checkCondition(configList: List<ConfigurationSection>) = configList.all { checkCondition( it) }
 
