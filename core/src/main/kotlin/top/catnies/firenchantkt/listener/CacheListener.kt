4059508d833f5a4b37b5e05d39150433968b02e2
Catnies <catnies@foxmail.com>
2025-07-31 17:18:50 +0800

    缓存监听器

--- core/src/main/kotlin/top/catnies/firenchantkt/listener/CacheListener.kt
+++ core/src/main/kotlin/top/catnies/firenchantkt/listener/CacheListener.kt
@@ -4,18 +4,37 @@ import kotlinx.coroutines.launch
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import top.catnies.firenchantkt.FirEnchantPlugin
+import top.catnies.firenchantkt.database.FirCacheManager
 import top.catnies.firenchantkt.database.FirConnectionManager
 
 class CacheListener: Listener {
 
     val plugin = FirEnchantPlugin.instance
-    val enchantLogData = FirConnectionManager.getInstance().enchantLogData
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
         plugin.launch {
-            val logCache = enchantLogData.getByPlayerRecent(event.player.uniqueId, 20)
+            // 铁砧附魔历史记录
+            val anvilLogCache = FirConnectionManager.getInstance().enchantLogData.getByPlayerRecent(event.player.uniqueId, 20)
+            anvilLogCache.forEach {
+                FirCacheManager.getInstance().addEnchantLog(player.uniqueId, it)
+            }
+            // 附魔台历史记录
+            val tableLogCache = FirConnectionManager.getInstance().enchantingHistoryData.getByPlayerRecent(event.player.uniqueId, 20)
+            tableLogCache.forEach {
+                FirCacheManager.getInstance().addEnchantingHistory(event.player.uniqueId, it)
+            }
+        }
+    }
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        val player = event.player
+        plugin.launch {
+            FirCacheManager.getInstance().clearPlayerCache(player.uniqueId)
         }
     }
 }
